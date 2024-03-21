b262046972c58a9ecbad298ecc56b49d8eb213bc
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2024-03-21 08:43:46 +0100

    Add simple parsing tool for mysql log files

---
+++ record/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"time"
+)
+
+var (
+	patterns = []string{"insert into job", "update job"}
+)
+
+func main() {
+	fmt.Print("Press Enter to start recording...")
+	_, _ = fmt.Scanln()
+
+	recordingStartedAt := time.Now()
+	fmt.Printf("Recording started at  %v. Press Enter to stop recording...\n", recordingStartedAt)
+
+	filePath := os.Args[1]
+	readFile, _ := os.Open(filePath)
+	defer readFile.Close()
+
+	go checkExit()
+	reader := bufio.NewReader(readFile)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				time.Sleep(500 * time.Millisecond)
+				continue
+			}
+
+			break
+		}
+		ts, validTimestamp := containsValidTimestamp(line)
+		if validTimestamp && matchesRecordingPeriod(ts, recordingStartedAt) && matchesPattern(line) {
+			fmt.Println(line)
+		}
+	}
+}
+
+func checkExit() {
+	var b = make([]byte, 1)
+	l, _ := os.Stdin.Read(b)
+	if l > 0 {
+		os.Exit(0)
+	}
+}
+func matchesRecordingPeriod(ts time.Time, startDate time.Time) bool {
+	return ts.Equal(startDate) || ts.After(startDate)
+}
+
+func containsValidTimestamp(line string) (time.Time, bool) {
+	split := strings.Split(line, "\t")
+	if len(split) == 0 {
+		return time.Time{}, false
+	}
+
+	d, err := time.Parse(time.RFC3339Nano, split[0])
+	if err != nil {
+		return time.Time{}, false
+	}
+	return d, true
+
+}
+
+func matchesPattern(line string) bool {
+	for _, pattern := range patterns {
+		if strings.Contains(line, pattern) {
+			return true
+		}
+	}
+	return false
+}
