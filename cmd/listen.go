e3e90ed7ef467b288a0ae39572fa4ba5ae3df074
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2024-03-29 12:46:31 +0100

    Move Listener to listen file and add some docs

--- cmd/listen.go
+++ cmd/listen.go
@@ -4,8 +4,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
+	"github.com/rwirdemann/databasedragon/adapter"
 	"github.com/rwirdemann/databasedragon/config"
+	"github.com/rwirdemann/databasedragon/matcher"
+	"github.com/rwirdemann/databasedragon/ticker"
+	"github.com/rwirdemann/databasedragon/validation"
 	"github.com/spf13/cobra"
 )
 
@@ -13,6 +18,71 @@ func init() {
 	listenCmd.Flags().String("expectations", "", "Filename with expectations")
 	listenCmd.MarkFlagRequired("expectations")
 	rootCmd.AddCommand(listenCmd)
+}
+
+type Listener struct {
+	config              config.Config
+	expectationFilename string
+	running             bool
+	validator           validation.Validator
+}
+
+func NewListener(c config.Config, expectationFilename string) *Listener {
+	return &Listener{config: c, expectationFilename: expectationFilename, running: false}
+}
+
+// Start starts listening by checking each new logfile entry against the expectations from the
+// expecations file. Matching expectations are removed. The listening counts as validated if all
+// expectations were met and removed. The caller should stop the listening by calling
+// Listener.Stop().
+func (l *Listener) Start() {
+	l.running = true
+	t := ticker.Ticker{}
+	t.Start()
+	log.Printf("Listening started at %v. Press Enter to stop listening...\n", t.GetStart())
+	expecations, err := os.ReadFile(l.expectationFilename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	l.validator = validation.NewUnorderedRemovalValidator(strings.Split(string(expecations), "\n"))
+
+	logPort := adapter.NewMYSQLLog(l.config.Filename)
+	defer logPort.Close()
+
+	m := matcher.NewDynamicDataMatcher(l.config)
+	for {
+		line, err := logPort.NextLine()
+		if err != nil {
+			log.Fatal(err)
+		}
+		ts, err := logPort.Timestamp(line)
+		if err != nil {
+			continue
+		}
+		if t.MatchesRecordingPeriod(ts) {
+			matches := false
+			matchingExpectation := ""
+			if m.MatchesAny(line) {
+				for _, e := range l.validator.GetExpectations() {
+					if m.MatchesExactly(line, e) {
+						log.Printf("Expectation met...: %s", line[:60])
+						matches = true
+						matchingExpectation = e
+						break
+					}
+				}
+			}
+			if matches {
+				l.validator.Remove(matchingExpectation)
+			}
+		}
+	}
+}
+
+// Stop stops the listening and validation loop.
+func (l *Listener) Stop() {
+	log.Println("Listening stoped")
+	l.validator.PrintResults()
 }
 
 var listener *Listener
