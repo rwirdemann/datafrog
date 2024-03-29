9d83708ef6efaf3b330f1f5fcec0729f403c6db8
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2024-03-29 11:19:41 +0100

    Add some more fuzzy matching tests

--- fuzzy/main.go
+++ fuzzy/main.go
@@ -7,11 +7,14 @@ import (
 )
 
 func main() {
-	s1 := "update job set description='World', publish_at='2024-03-28 10:57:27', publish_trials=1, published_timestamp='2024-03-28 10:57:36.261562', tags='', title='Hello' where id=11"
-	s2 := "update job set description='World', publish_at='2024-03-28 10:57:27', publish_trials=1, published_timestamp='2024-03-28 10:57:36.261562', tags='', title='Hello' where id=11"
-	distance := float64(levenshtein.ComputeDistance(s1, s2))
-	sum := float64(len(s1)) + float64(len(s2))
-	ratio := (sum - distance) / sum
 
-	fmt.Printf("The distance / ratio is %f %f.\n", distance, ratio)
+	s1 := "update job set description='World', publish_at='<DATE_STR>', publish_trials=1, published_timestamp='<DATE_STR>', tags='', title='Hello' where id=11"
+	for i := 1000; i < 1200; i++ {
+		s2 := fmt.Sprintf("update job set description='World', publish_at='<DATE_STR>', publish_trials=1, published_timestamp='<DATE_STR>', tags='', title='Hello' where id=%d", i)
+		distance := float64(levenshtein.ComputeDistance(s1, s2))
+		sum := float64(len(s1)) + float64(len(s2))
+		ratio := (sum - distance) / sum
+
+		fmt.Printf("Distance: %f\tRatio: %f\n", distance, ratio)
+	}
 }
