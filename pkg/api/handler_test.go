7dc3cca026c766910b6fbf232515a070fe3bd547
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2024-06-24 13:39:13 +0200

    Add TestVerification

--- pkg/api/handler_test.go
+++ pkg/api/handler_test.go
@@ -24,7 +24,7 @@ func TestStartRecordingNoChannels(t *testing.T) {
 	assert.Equal(t, http.StatusFailedDependency, rr.Code)
 }
 
-func TestStartStopRecording(t *testing.T) {
+func TestRecording(t *testing.T) {
 	config.Channels = append(config.Channels, df.Channel{})
 	logFactory := mocks.LogFactory{}
 	repository := &mocks.TestRepository{}
@@ -40,6 +40,20 @@ func TestStartStopRecording(t *testing.T) {
 	assert.Equal(t, "create-job", tc.Name)
 }
 
+func TestVerification(t *testing.T) {
+	config.Channels = append(config.Channels, df.Channel{})
+	logFactory := mocks.LogFactory{}
+	repository := &mocks.TestRepository{Testcases: []df.Testcase{{Name: testname}}}
+	rr := startVerification(t, logFactory, repository)
+	assert.Equal(t, http.StatusAccepted, rr.Code)
+	runner, ok := verifyRunners[testname]
+	assert.True(t, ok)
+	err := runner.Stop()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
 func startRecording(t *testing.T, logFactory df.LogFactory, repository df.TestRepository) *httptest.ResponseRecorder {
 	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("/tests/%s/recordings", testname), nil)
 	if err != nil {
@@ -48,6 +62,18 @@ func startRecording(t *testing.T, logFactory df.LogFactory, repository df.TestRe
 	rr := httptest.NewRecorder()
 	r := mux.NewRouter()
 	r.HandleFunc("/tests/{name}/recordings", StartRecording(logFactory, repository)).Methods("POST")
+	r.ServeHTTP(rr, req)
+	return rr
+}
+
+func startVerification(t *testing.T, logFactory df.LogFactory, repository df.TestRepository) *httptest.ResponseRecorder {
+	req, err := http.NewRequest(http.MethodPut, fmt.Sprintf("/tests/%s/verifications", testname), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	r := mux.NewRouter()
+	r.HandleFunc("/tests/{name}/verifications", StartVerify(logFactory, repository)).Methods("PUT")
 	r.ServeHTTP(rr, req)
 	return rr
 }
