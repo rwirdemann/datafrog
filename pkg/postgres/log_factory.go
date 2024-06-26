839d8210dfe19dbf35896ef65b6a1e58628fc159
Reinhard Wegner <reinhard.wegner@codekeepers.de>
2024-06-26 13:57:45 +0200

    Moved date-check to an own function (with error handling and return-early)

--- pkg/postgres/log_factory.go
+++ pkg/postgres/log_factory.go
@@ -1,6 +1,8 @@
 package postgres
 
 import (
+	"io/fs"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -11,31 +13,53 @@ import (
 type LogFactory struct {
 }
 
-func (f LogFactory) Create(logFilePath string) df.Log {
-	var dateString = "YYYY-MM-DD_hhmmss.log"
-	if strings.Contains(logFilePath, dateString) {
+func (f LogFactory) Create(filename string) df.Log {
+	logFilePath, err := resolveDate(filename)
+	if err != nil {
+		log.Fatalf("LogFactory: Could not resolve Log-File %s: %s", filename, err)
+	}
+	return NewPostgresLog(logFilePath)
+}
 
-		// Logfile name contains a timestamp: Find the newest one in containing folder
-		path := filepath.Dir(logFilePath)
-		var expectedFileNameStart string = logFilePath[len(path)+1 : len(logFilePath)-len(dateString)]
-		entries, err := os.ReadDir(path)
-		if err == nil {
-			var newestTime int64 = 0
-			for _, file := range entries {
-				// First check, if the name matches
-				if strings.Contains(file.Name(), expectedFileNameStart) {
-					// Now check, if it is the newest
-					fi, err := os.Stat(path + "/" + file.Name())
-					if err == nil {
-						currTime := fi.ModTime().Unix()
-						if currTime > newestTime {
-							newestTime = currTime
-							logFilePath = path + "/" + file.Name()
-						}
-					}
+func resolveDate(filename string) (string, error) {
+
+	var dateString = "YYYY-MM-DD_hhmmss.log"
+	if !strings.Contains(filename, dateString) {
+		return filename, nil
+	}
+
+	// Logfile name contains a timestamp: Find the newest one in containing folder
+
+	path := filepath.Dir(filename)
+
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return filename, err
+	}
+
+	var expectedFileNameStart string = filename[len(path)+1 : len(filename)-len(dateString)]
+	var logFilePath = filename
+	var newestTime int64 = 0
+	var found = false
+	for _, file := range entries {
+		// First check, if the name matches
+		if strings.Contains(file.Name(), expectedFileNameStart) {
+			// Now check, if it is the newest
+			fi, err := os.Stat(path + "/" + file.Name())
+			if err == nil {
+				currTime := fi.ModTime().Unix()
+				if currTime > newestTime {
+					found = true
+					newestTime = currTime
+					logFilePath = path + "/" + file.Name()
 				}
 			}
 		}
 	}
-	return NewPostgresLog(logFilePath)
+
+	err = nil
+	if !found {
+		err = fs.ErrNotExist
+	}
+	return logFilePath, err
 }
